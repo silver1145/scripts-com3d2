cc4df428dd2c7015cbfaa5022c3c2c924fa943a7
silver1145 <139576933+silver1145@users.noreply.github.com>
2024-03-13 22:21:02 +0800

    update

--- scripts/inoutanim_sync.cs
+++ scripts/inoutanim_sync.cs
@@ -1,6 +1,6 @@
 // #author silver1145
-// #name Syasei Sync With Inoutanim
-// #desc Vym Syasei Sync With Inoutanim
+// #name InoutAnimSync
+// #desc InoutAnim Sync
 // #ref ${BepInExRoot}/../Sybaris/UnityInjector/COM3D2.InOutAnimation.Plugin.dll
 
 using UnityEngine;
@@ -16,26 +16,44 @@ using System.Reflection.Emit;
 
 public static class InoutAnimSync
 {
-
     static Harmony instance;
+    static readonly int BUFFERSIZE = 100;
     static float shotReadyTime = 0f;
     static float rate;
+    static float originRate;
+    static float smoothRate;
+    static float relativeRate;
+    static int rateBufferIndex = 0;
+    static float[] rateBuffer = new float[BUFFERSIZE];
+    static LinkedList<float> rateMinQueue = new LinkedList<float>();
+    static LinkedList<float> rateMaxQueue = new LinkedList<float>();
+    static float rateMin = 0f;
+    static float rateMax = 0f;
+    static float relativeRateMin = 0f;
+    static float relativeRateMax = 0f;
+    static float relativeRateFactor = 1f;
     // Config
     static public bool bindConfig = false;
     static public ConfigFile configFile = new ConfigFile(Path.Combine(BepInEx.Paths.ConfigPath, "InoutAnimSync.cfg"), false);
     static public ConfigEntry<bool> _enableEmissionAnim = configFile.Bind("InoutAnimSync Setting", "EnableEmissionAnim", false, "Enable Emission Anim");
     static public ConfigEntry<float> _emissionIntensityStart = configFile.Bind("InoutAnimSync Setting", "IntensityStart", 0f, "Intensity Start");
     static public ConfigEntry<float> _emissionIntensityEnd = configFile.Bind("InoutAnimSync Setting", "IntensityEnd", 0f, "Intensity End");
+    static public ConfigEntry<float> _emissionThresholdStart = configFile.Bind("InoutAnimSync Setting", "ThresholdStart", 0f, "Threshold Start");
+    static public ConfigEntry<float> _emissionThresholdEnd = configFile.Bind("InoutAnimSync Setting", "ThresholdEnd", 0f, "Threshold End");
     static public ConfigEntry<int> _smoothWindowSize = configFile.Bind("InoutAnimSync Setting", "SmoothWindowSize", 0, "Smooth Window Size");
+    static public ConfigEntry<bool> _useRelativeInterval = configFile.Bind("InoutAnimSync Setting", "UseRelativeInterval", false, "Use Relative Interval");
+    static public ConfigEntry<float> _relativeExponent = configFile.Bind("InoutAnimSync Setting", "RelativeExponent", 0.3f, "Relative Exponent");
     static public ConfigEntry<bool> _enableAheAnim = configFile.Bind("InoutAnimSync Setting", "EnableAheAnim", false, "Enable Ahe Anim");
     // Emission Anim
-    static int smoothIndex;
-    static float[] smoothBuffer = new float[100];
     static bool enableEmissionAnim = _enableEmissionAnim.Value;
-    static int smoothWindowSizeValue = Math.Min(_smoothWindowSize.Value, 100);
+    static int smoothWindowSizeValue = Math.Min(_smoothWindowSize.Value, BUFFERSIZE);
     static float emissionIntensityStart = _emissionIntensityStart.Value;
     static float emissionIntensityEnd = _emissionIntensityEnd.Value;
-    static int smoothWindowSize { get { return smoothWindowSizeValue; } set { smoothWindowSizeValue = Math.Min(value, 100); } }
+    static float emissionThresholdStart = _emissionThresholdStart.Value;
+    static float emissionThresholdEnd = _emissionThresholdEnd.Value;
+    static int smoothWindowSize { get { return smoothWindowSizeValue; } set { smoothWindowSizeValue = Math.Min(value, BUFFERSIZE); } }
+    static bool useRelativeInterval = _useRelativeInterval.Value;
+    static float relativeExponent = _relativeExponent.Value;
     // Ahe Anim
     static bool enableAheAnim = _enableAheAnim.Value;
     static Maid curMaid;
@@ -49,7 +67,11 @@ public static class InoutAnimSync
         _enableEmissionAnim.SettingChanged += (s, e) => enableEmissionAnim = _enableEmissionAnim.Value;
         _emissionIntensityStart.SettingChanged += (s, e) => emissionIntensityStart = _emissionIntensityStart.Value;
         _emissionIntensityEnd.SettingChanged += (s, e) => emissionIntensityEnd = _emissionIntensityEnd.Value;
+        _emissionThresholdStart.SettingChanged += (s, e) => emissionThresholdStart = _emissionThresholdStart.Value;
+        _emissionThresholdEnd.SettingChanged += (s, e) => emissionThresholdEnd = _emissionThresholdEnd.Value;
         _smoothWindowSize.SettingChanged += (s, e) => smoothWindowSize = _smoothWindowSize.Value;
+        _useRelativeInterval.SettingChanged += (s, e) => useRelativeInterval = _useRelativeInterval.Value;
+        _relativeExponent.SettingChanged += (s, e) => relativeExponent = _relativeExponent.Value;
         _enableAheAnim.SettingChanged += (s, e) => { enableAheAnim = _enableAheAnim.Value; TryPatchVYMEnhance.UpdateAheAnimState(); };
     }
 
@@ -57,19 +79,92 @@ public static class InoutAnimSync
     {
         instance.UnpatchAll(instance.Id);
         instance = null;
+        curMaid = null;
+        rateBuffer = null;
+        rateMinQueue = null;
+        rateMaxQueue = null;
+        configFile = null;
+        _enableEmissionAnim = null;
+        _emissionIntensityStart = null;
+        _emissionIntensityEnd = null;
+        _emissionThresholdStart = null;
+        _emissionThresholdEnd = null;
+        _smoothWindowSize = null;
+        _useRelativeInterval = null;
+        _relativeExponent = null;
+        _enableAheAnim = null;
     }
 
-    static float SmoothValue(float inputValue)
+    static void SetRate(float value)
     {
-        smoothBuffer[smoothIndex] = inputValue;
-        float sum = 0;
-        for (int i = 0; i < smoothWindowSize; i++)
+        rate = originRate = value;
+        if (smoothWindowSize > 0)
         {
-            sum += smoothBuffer[i];
+            float sum = value;
+            for (int i = 0, j = (rateBufferIndex - smoothWindowSize + BUFFERSIZE) % BUFFERSIZE; i < smoothWindowSize - 1; i++, j = (j + 1) % smoothWindowSize)
+            {
+                sum += rateBuffer[j];
+            }
+            smoothRate = sum / smoothWindowSize;
+            rate = smoothRate;
         }
-        float smoothedValue = sum / smoothWindowSize;
-        smoothIndex = (smoothIndex + 1) % smoothWindowSize;
-        return smoothedValue;
+        if (useRelativeInterval)
+        {
+            bool needUpdate = false;
+            float popValue = rateBuffer[rateBufferIndex];
+            if (popValue != 0f)
+            {
+                // Queue Full
+                if (popValue == rateMin)
+                {
+                    needUpdate = true;
+                    rateMinQueue.RemoveFirst();
+                }
+                if (popValue == rateMax)
+                {
+                    needUpdate = true;
+                    rateMaxQueue.RemoveFirst();
+                }
+            }
+            while (rateMinQueue.Count != 0 && rateMinQueue.Last.Value > value)
+            {
+                rateMinQueue.RemoveLast();
+            }
+            rateMinQueue.AddLast(value);
+            needUpdate |= rateMinQueue.First.Value != rateMin;
+            while (rateMaxQueue.Count != 0 && rateMaxQueue.Last.Value < value)
+            {
+                rateMaxQueue.RemoveLast();
+            }
+            rateMaxQueue.AddLast(value);
+            needUpdate |= rateMaxQueue.First.Value != rateMax;
+            if (needUpdate)
+            {
+                rateMin = rateMinQueue.First.Value;
+                rateMax = rateMaxQueue.First.Value;
+                // [rateMin, rateMax] => [relativeRateMin, relativeRateMax]
+                // length => length^relativeExponent
+                // relativeRateMin = (1 - length^relativeExponent)/(1 - length) * rateMin
+                // relativeRateMax = 1 - (1 - length^relativeExponent)/(1 - length) * (1 - rateMax)
+                float length = Math.Max(rateMax - rateMin, 0.05f);
+                if (length >= 0.95f)
+                {
+                    relativeRateMin = rateMin;
+                    relativeRateMax = rateMax;
+                }
+                else
+                {
+                    float factor = (1f - (float)Math.Pow(length, relativeExponent)) / (1f - length);
+                    relativeRateMin = factor * Math.Min(rateMin, 0.95f);
+                    relativeRateMax = 1 - factor * (1f - Math.Max(rateMax, 0.05f));
+                }
+                relativeRateFactor = (relativeRateMax - relativeRateMin) / length;
+            }
+            relativeRate = ((smoothWindowSize > 0 ? smoothRate : value) - rateMin) * relativeRateFactor + relativeRateMin;
+            rate = relativeRate;
+        }
+        rateBuffer[rateBufferIndex] = value;
+        rateBufferIndex = (rateBufferIndex + 1) % BUFFERSIZE;
     }
 
     public static bool IsShootReady()
@@ -111,12 +206,12 @@ public static class InoutAnimSync
             {
                 if (r >= 0.01f)
                 {
-                    rate = r;
+                    SetRate(r);
+                    TryPatchSceneCapture.UpdateRate();
+                    TryPatchVYMEnhance.UpdateRate();
                     break;
                 }
             }
-            TryPatchSceneCapture.UpdateRate();
-            TryPatchVYMEnhance.UpdateRate();
             if (__instance.TargetMaid != curMaid && TryPatchVYMEnhance.SetMaid(__instance.TargetMaid))
             {
                 curMaid = __instance.TargetMaid;
@@ -142,10 +237,26 @@ public static class InoutAnimSync
         {
             _emissionIntensityEnd.Value = f;
         }, false));
-        __instance.containers["emissionanim"].Add(new InOutAnimation.Controller.LabelSlider("Smooth WinSize", (float)smoothWindowSize, 0f, 10f, 0f, delegate (float f)
+        __instance.containers["emissionanim"].Add(new InOutAnimation.Controller.LabelSlider("Threshold Start", emissionThresholdStart, 0f, 3f, 0f, delegate (float f)
+        {
+            _emissionThresholdStart.Value = f;
+        }, false));
+        __instance.containers["emissionanim"].Add(new InOutAnimation.Controller.LabelSlider("Threshold End", emissionThresholdEnd, 0f, 3f, 0f, delegate (float f)
+        {
+            _emissionThresholdEnd.Value = f;
+        }, false));
+        __instance.containers["emissionanim"].Add(new InOutAnimation.Controller.LabelSlider("Smooth WinSize", smoothWindowSize, 0f, 20f, 0f, delegate (float f)
         {
             _smoothWindowSize.Value = (int)f;
         }, true));
+        __instance.containers["emissionanim"].Add(new InOutAnimation.Controller.ToggleButton("Relative Interval", useRelativeInterval, delegate (bool b)
+        {
+            _useRelativeInterval.Value = b;
+        }));
+        __instance.containers["emissionanim"].Add(new InOutAnimation.Controller.LabelSlider("Relative Exponent", relativeExponent, 0.1f, 0.9f, 0.2f, delegate (float f)
+        {
+            _relativeExponent.Value = f;
+        }, false));
         __instance.containers.Add("aheanim", new InOutAnimation.Controller.Container("AheEyeAnim"));
         __instance.containers["aheanim"].Add(new InOutAnimation.Controller.ToggleButton("有効", enableAheAnim, delegate (bool b)
         {
@@ -173,10 +284,26 @@ public static class InoutAnimSync
             {
                 _emissionIntensityEnd.Value = f;
             }, false);
-            contents[3] = new InOutAnimation.Controller.LabelSlider("Smooth WinSize", (float)smoothWindowSize, 0f, 10f, 0f, delegate (float f)
+            contents[3] = new InOutAnimation.Controller.LabelSlider("Threshold Start", emissionThresholdStart, 0f, 3f, 0f, delegate (float f)
+            {
+                _emissionThresholdStart.Value = f;
+            }, false);
+            contents[4] = new InOutAnimation.Controller.LabelSlider("Threshold End", emissionThresholdEnd, 0f, 3f, 0f, delegate (float f)
+            {
+                _emissionThresholdEnd.Value = f;
+            }, false);
+            contents[5] = new InOutAnimation.Controller.LabelSlider("Smooth WinSize", smoothWindowSize, 0f, 20f, 0f, delegate (float f)
             {
                 _smoothWindowSize.Value = (int)f;
             }, true);
+            contents[6] = new InOutAnimation.Controller.ToggleButton("Relative Interval", useRelativeInterval, delegate (bool b)
+            {
+                _useRelativeInterval.Value = b;
+            });
+            contents[7] = new InOutAnimation.Controller.LabelSlider("Relative Exponent", relativeExponent, 0.1f, 0.9f, 0.2f, delegate (float f)
+            {
+                _relativeExponent.Value = f;
+            }, false);
             contents = __instance.controller.containers["aheanim"].contents;
             contents[0] = new InOutAnimation.Controller.ToggleButton("有効", enableAheAnim, delegate (bool b)
             {
@@ -240,9 +367,11 @@ public static class InoutAnimSync
     class TryPatchSceneCapture : TryPatch
     {
         public static bool valid = false;
+        public static object cinematicBloomLayer;
         public static Type cinematicBloomLayerDef;
         public static FieldInfo fieldCinematicBloomLayer;
         public static FieldInfo fieldEmissionIntensity;
+        public static FieldInfo fieldEmissionThreshold;
         public TryPatchSceneCapture(Harmony harmony, int failLimit = 3) : base(harmony, failLimit) { }
 
         public override bool Patch()
@@ -255,6 +384,7 @@ public static class InoutAnimSync
             }
             fieldCinematicBloomLayer = AccessTools.Field(_cinematicBloomLayerDef, "cinematicBloomEffect");
             fieldEmissionIntensity = AccessTools.Field(_cinematicBloomLayer, "_intensity");
+            fieldEmissionThreshold = AccessTools.Field(_cinematicBloomLayer, "_threshold");
             cinematicBloomLayerDef = _cinematicBloomLayerDef;
             valid = true;
             return true;
@@ -262,10 +392,13 @@ public static class InoutAnimSync
 
         public static float? GetEmissionIntensity()
         {
-            object layer = fieldCinematicBloomLayer.GetValue(null);
-            if (layer != null)
+            if (cinematicBloomLayer == null)
             {
-                object intensity = fieldEmissionIntensity.GetValue(layer);
+                cinematicBloomLayer = fieldCinematicBloomLayer.GetValue(null);
+            }
+            if (cinematicBloomLayer != null)
+            {
+                object intensity = fieldEmissionIntensity.GetValue(cinematicBloomLayer);
                 if (intensity != null)
                 {
                     return (float)intensity;
@@ -276,10 +409,44 @@ public static class InoutAnimSync
 
         public static bool SetEmissionIntensity(float value)
         {
-            object layer = fieldCinematicBloomLayer.GetValue(null);
-            if (layer != null)
+            if (cinematicBloomLayer == null)
             {
-                fieldEmissionIntensity.SetValue(layer, value);
+                cinematicBloomLayer = fieldCinematicBloomLayer.GetValue(null);
+            }
+            if (cinematicBloomLayer != null)
+            {
+                fieldEmissionIntensity.SetValue(cinematicBloomLayer, value);
+                return true;
+            }
+            return false;
+        }
+
+        public static float? GetEmissionThreshold()
+        {
+            if (cinematicBloomLayer == null)
+            {
+                cinematicBloomLayer = fieldCinematicBloomLayer.GetValue(null);
+            }
+            if (cinematicBloomLayer != null)
+            {
+                object threshold = fieldEmissionThreshold.GetValue(cinematicBloomLayer);
+                if (threshold != null)
+                {
+                    return (float)threshold;
+                }
+            }
+            return null;
+        }
+
+        public static bool SetEmissionThreshold(float value)
+        {
+            if (cinematicBloomLayer == null)
+            {
+                cinematicBloomLayer = fieldCinematicBloomLayer.GetValue(null);
+            }
+            if (cinematicBloomLayer != null)
+            {
+                fieldEmissionThreshold.SetValue(cinematicBloomLayer, value);
                 return true;
             }
             return false;
@@ -287,14 +454,8 @@ public static class InoutAnimSync
 
         public static void UpdateRate()
         {
-            if (smoothWindowSize > 0)
-            {
-                SetEmissionIntensity(Mathf.Lerp(emissionIntensityStart, emissionIntensityEnd, SmoothValue(rate)));
-            }
-            else
-            {
-                SetEmissionIntensity(Mathf.Lerp(emissionIntensityStart, emissionIntensityEnd, rate));
-            }
+            SetEmissionIntensity(Mathf.Lerp(emissionIntensityStart, emissionIntensityEnd, rate));
+            SetEmissionThreshold(Mathf.Lerp(emissionThresholdStart, emissionThresholdEnd, rate));
         }
     }
 
